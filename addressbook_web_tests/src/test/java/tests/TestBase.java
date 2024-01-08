0fd117018ffbbbf127e489592b76998d24b710c0
iloiza <ekaterina.popova93@gmail.com>
2024-01-08 10:55:20 +0300

    Лекция 4.5

--- addressbook_web_tests/src/test/java/tests/TestBase.java
+++ addressbook_web_tests/src/test/java/tests/TestBase.java
@@ -16,14 +16,13 @@ public class TestBase {
         }
     }
 
-    public String randomString(int n) {
+    public static String randomString(int n) {
         var rnd = new Random();
         var result = "";
 
         for (int i = 0; i < n; i++){
             result = result + (char)('a' + rnd.nextInt(26));
         }
-
         return result;
     }
 
