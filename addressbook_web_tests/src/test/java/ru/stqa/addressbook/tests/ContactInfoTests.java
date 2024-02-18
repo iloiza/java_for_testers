abe3395952011cbf88b2558666a3e1227a687e45
iloiza <ekaterina.popova93@gmail.com>
2024-02-18 14:38:34 +0300

    Лекция 10.1 Исправленный код(3)

--- addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactInfoTests.java
+++ addressbook_web_tests/src/test/java/ru/stqa/addressbook/tests/ContactInfoTests.java
@@ -30,7 +30,7 @@ public class ContactInfoTests extends TestBase {
             var contacts = app.hbm().getContactList();
             var expected = contacts.stream().collect(Collectors.toMap(ContactData::id, contact ->
                     Stream.of(contact.home(), contact.mobile(), contact.work())//, contact.fax())
-                            .filter(s -> s != null && !"".equals(s))
+                            .filter(s -> s != null && !s.isEmpty())
                             .collect(Collectors.joining("\n"))
             ));
             var phones = app.contacts().getPhones();
